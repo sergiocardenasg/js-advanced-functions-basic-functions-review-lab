fbc9a68a1699fd9618d08dba41846e8701bfad31
sergiocardenasg <sergiocardenas@utexas.edu>
2021-07-06 22:55:56 -0500

    commit tree eb2d3f56d9e942eedfd5797bdfd3f38053a283ad

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+};
+
+const mondayWork = function (activity = "go to the office"){
+    return `This Monday, I will ${activity}.`;
+};
+
+function wrapAdjective(flair = "*") {
+    return function(para = "special") {
+      return `You are ${flair}${para}${flair}!`;
+    }
+};
+
+var Calculator = {
+    add: function(a,b) {
+        return a + b;
+    },
+    subtract: function(a,b) {
+        return a - b;
+    },
+    multiply: function(a,b) {
+        return a * b;
+    },
+    divide: function(a,b) {
+        return a / b;
+    }
+};
+
+function actionApplyer(start, arr) {
+    let action = start;
+
+    for (let i = 0; i < arr.length; i++) {
+      action = arr[i](action)
+    };
+  
+    return action;
+};
